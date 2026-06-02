9692a0d1bb8d8a6dd7a464db0c2819357412bb6c
Your Name <you@example.com>
2026-06-02 10:46:20 +0800

    init

--- tempt.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-data = np.load("/media/a/新加卷/hanfeng/segment_sub_output/BEAM_1aEEYa00Ed5Z4sE34qDJKu_part/seam_24.pkl", allow_pickle=True)
-
-print(1)
--- vis.sh
+++
@@ -1,3 +0,0 @@
-/home/a/miniforge3/envs/env_isaaclab/bin/python /home/a/Projects/Github/gt_gen_hanfeng/visualize_seam_scene.py \
-    --usd "/media/a/新加卷/hanfeng/segment_sub_output/BEAM_1aEEYa00Ed5Z4sE34qDJKu_part/BEAM_1aEEYa00Ed5Z4sE34qDJKu_part.usd" \
-    --pkl "/media/a/新加卷/hanfeng/segment_sub_output/BEAM_1aEEYa00Ed5Z4sE34qDJKu_part/seam_0.pkl"
